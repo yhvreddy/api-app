3cbdba29526ac0e8d8029b7dd8543a52ad73441f
yhvreddy <yhvreddyinfo@gmail.com>
2024-02-16 07:37:07 +0530

    Added updated UserUpdateRequest page

---
+++ app/Http/Requests/Users/v1/UserUpdateRequest.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Requests\Users\v1;
+
+use Illuminate\Foundation\Http\FormRequest;
+use App\Http\Traits\HttpResonse;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Validation\Rule;
+use Hash;
+
+class UserUpdateRequest extends FormRequest
+{
+    use HttpResonse;
+
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+
+        $method = $this->method();
+
+        return [
+            'name' => 'required|alpha_spaces',
+            'email' => [
+                        'sometimes',
+                        'email',
+                        Rule::unique('users')->ignore($this->user->id, 'id', 'App\Models\User')
+                    ],
+            'password' => 'sometimes'
+        ];
+    }
+
+    public function messages(){
+        return [
+            'alpha_spaces' => 'The :attribute may only contain letters and spaces.',
+        ];
+    }
+
+    // Customize the validation failure behavior
+    protected function failedValidation(Validator $validator){
+        if ($this->wantsJson()) {
+            throw new HttpResponseException(
+                $this->validation('Validation error', $validator->errors())
+            );
+        } else {
+            throw new HttpResponseException(
+                redirect()->back()
+                    ->withInput($this->input())
+                    ->withErrors($validator)
+            );
+        }
+    }
+
+    /**
+     * Prepare the data for validation.
+     */
+    protected function prepareForValidation(): void
+    {
+        if($this->password == null){
+            $this->request->remove('password');
+        }
+        if($this->email == null){
+            $this->request->remove('email');
+        }
+    }
+}
