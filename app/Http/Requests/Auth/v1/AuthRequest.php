cf4ce80ff1ff752ebdd14da12ac2c7c7ed130a7a
yhvreddy <yhvreddyinfo@gmail.com>
2024-02-16 10:41:55 +0530

    Added New Form Request Page

---
+++ app/Http/Requests/Auth/v1/AuthRequest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Requests\Auth\v1;
+
+use Illuminate\Foundation\Http\FormRequest;
+use App\Http\Traits\HttpResonse;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\JsonResponse;
+
+class AuthRequest extends FormRequest
+{
+    use HttpResonse;
+    
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => 'required|email',
+            'password' => 'required'
+        ];
+    }
+
+    // Customize the validation failure behavior
+    protected function failedValidation(Validator $validator){
+        if ($this->wantsJson()) {
+            throw new HttpResponseException(
+                $this->validation('Validation error', $validator->errors())
+            );
+        } else {
+            throw new HttpResponseException(
+                redirect()->back()
+                    ->withInput($this->input())
+                    ->withErrors($validator)
+            );
+        }
+    }
+}
